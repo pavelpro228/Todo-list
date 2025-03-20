a377cc17433dbb9707fbf79825e25cf756daa14c
Pavel Buylin <buylin399pavel@gmail.com>
2025-03-20 12:02:56 +0200

    ...

---
+++ src/Test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Test = () => {
+    const func = () => {
+        let variable = `123`
+    }
+  return (
+    <>
+        <div className='bg-'></div>
+    </>
+  )
+}
+
+export default Test
